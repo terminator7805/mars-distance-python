1b2337df3148ac22ea42d70b9096ebcaaef44b4a
acabezafra1@educacion.navarra.es <acabezafra1@educacion.navarra.es>
2022-05-17 15:22:23 +0200

    Initial TKINTER GUI (17/05/2022)

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from tkinter import *
+window = Tk()
+#window.overrideredirect(True)
+window.title('Mars')
+window.iconphoto(False, PhotoImage(file='resources/3D_Mars.png'))
+text1 = Label(text='Prueba')
+text1.pack()
+
+def enviar():
+    prueba = Label(text="A")
+    prueba.pack()
+boton = Button(text="Enviar", command=enviar)
+boton.pack()
+window.mainloop()
