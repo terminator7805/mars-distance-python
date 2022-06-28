4e9ae95e437e17aa2986036b46437662265978a5
Terminator7805 <acabezafra1@educacion.navarra.es>
2022-06-28 14:54:20 +0200

    Changes to setInterval and the way distance updates

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #IMPORTS
-from re import T
 from tkinter import *
 from tkcalendar import Calendar, DateEntry
 import datetime;
@@ -7,16 +6,29 @@ from astropy.time import Time
 from astropy.coordinates import solar_system_ephemeris, EarthLocation
 from astropy.coordinates import get_body_barycentric, get_body, get_moon
 import astropy.units as u
-import threading
+import time, threading
+
+inter = None
+StartTime=time.time()
+
+class setInterval : #set interval requirements
+    def __init__(self,interval,action) :
+        self.interval=interval
+        self.action=action
+        self.stopEvent=threading.Event()
+        thread=threading.Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self) :
+        nextTime=time.time()+self.interval
+        while not self.stopEvent.wait(nextTime-time.time()) :
+            nextTime+=self.interval
+            self.action()
+
+    def cancel(self) :
+        self.stopEvent.set()
 
 #FUNCTIONS
-def set_interval(func, sec): #basic interval function
-    def func_wrapper():
-        set_interval(func, sec)
-        func()
-    t = threading.Timer(sec, func_wrapper)
-    t.start()
-    return t
 
 def separationattime(t): #gets the current distance from the earth to mars in KM
     converttime = Time(t)
@@ -28,43 +40,44 @@ def separationattime(t): #gets the current distance from the earth to mars in KM
     sep = mars.separation_3d(earth).to(u.km)
     return sep
 
-def distancebutton(): #Triggers the loop of the distance text, function for dbutton
+def distancebutton(): #Changes the distance text, function for date buttons
+    global inter
     if timeget.get() == 'now': 
         utc = Time.now()
         timenow = utc + datetime.timedelta(hours=2)
-        time = datetime.datetime.strptime(str(timenow), '%Y-%m-%d %H:%M:%S.%f')
-        msg_display.config(text=time.strftime('%d/%m/%Y %H:%M:%S'))
+        timed = datetime.datetime.strptime(str(timenow), '%Y-%m-%d %H:%M:%S.%f')
+        clock_display.config(text=timed.strftime('%d/%m/%Y %H:%M:%S'))
     else: 
-        time = datetime.datetime.strptime(timeget.get(), '%Y/%m/%d %H:%M:%S.%f')
-        msg_display.config(text=time.strftime('%d/%m/%Y %H:%M:%S'))
+        timed = datetime.datetime.strptime(timeget.get(), '%Y/%m/%d %H:%M:%S.%f')
+        clock_display.config(text=timed.strftime('%d/%m/%Y %H:%M:%S'))
     distance = StringVar()
-    distance.set(separationattime(time))
-    def update():
-        if timeget.get() == 'now': 
-            utc = Time.now()
-            timenow = utc + datetime.timedelta(hours=2)
-            time = datetime.datetime.strptime(str(timenow), '%Y-%m-%d %H:%M:%S.%f')
-            msg_display.config(text=time.strftime('%d/%m/%Y %H:%M:%S'))
-        else: 
-            time = datetime.datetime.strptime(timeget.get(), '%Y/%m/%d %H:%M:%S.%f')
-            msg_display.config(text=time.strftime('%d/%m/%Y %H:%M:%S'))
-    prueba = Label(textvariable=distance)
-    prueba.pack()
-    set_interval(update, 1)
+    distance.set(separationattime(timed))
+    distance_display.config(text=distance.get())
+    def update(): #Updates the realtime distance when needed
+        global inter
+        if timeget.get() != 'now': 
+            inter.cancel()
+            inter = None
+            return
+        utc = Time.now()
+        timenow = utc + datetime.timedelta(hours=2)
+        timed = datetime.datetime.strptime(str(timenow), '%Y-%m-%d %H:%M:%S.%f')
+        clock_display.config(text=timed.strftime('%d/%m/%Y %H:%M:%S'))
+        distance.set(separationattime(timed))
+        distance_display.config(text=distance.get())
+    if timeget.get() == 'now' and inter == None:  inter=setInterval(1,update) #Triggers the loop of update funct without stacking intervals
 
 #TKINTER
-# Add Calendar
-
- 
+#Window's settings
 window = Tk() 
 #window.overrideredirect(True)
 window.title('Mars')
+window.iconphoto(False, PhotoImage(file='resources/3D_Mars.png'))
+
+#Date select
 global timeget
 timeget = StringVar()
 timeget.set('now')
-
-
-window.iconphoto(False, PhotoImage(file='resources/3D_Mars.png'))
 today = datetime.date.today().strftime("%d/%m/%Y").split('/')
 hour_string=StringVar()
 min_string=StringVar()
@@ -79,9 +92,9 @@ ftwo.pack(pady=10)
 cal = Calendar(
     fone, 
     selectmode="day", 
-    year=2021, 
-    month=2,
-    day=3
+    year=int(today[2]), 
+    month=int(today[1]),
+    day=int(today[0])
     )
 cal.pack()
 
@@ -117,19 +130,6 @@ sec = Spinbox(
     font=f,
     justify=CENTER
     )
-def display_msg():
-    date = cal.get_date()
-    m = min_sb.get()
-    h = sec_hour.get()
-    s = sec.get()
-    if len(h) == 1:
-        h = '0' + h
-    if len(m) == 1:
-        m = '0' + m
-    if len(s) == 1:
-        s = '0' + s
-    timeget.set(f"{datetime.datetime.strptime(date, '%d/%m/%y').strftime('%Y/%m/%d')} {h}:{m}:{s}.0")
-       
 
 
 if last_value == "59" and min_string.get() == "0":
@@ -147,14 +147,28 @@ sec_hour.pack(side=LEFT, fill=X, expand=True)
 min_sb.pack(side=LEFT, fill=X, expand=True)
 sec.pack(side=LEFT, fill=X, expand=True)
 
+def setdatefromcal(): #Gets the info from the calendar and sends it to process and get the distance
+    date = cal.get_date()
+    m = min_sb.get()
+    h = sec_hour.get()
+    s = sec.get()
+    if len(h) == 1:
+        h = '0' + h
+    if len(m) == 1:
+        m = '0' + m
+    if len(s) == 1:
+        s = '0' + s
+    timeget.set(f"{datetime.datetime.strptime(date, '%d/%m/%y').strftime('%Y/%m/%d')} {h}:{m}:{s}.0")
+       
 
 
+#Buttons that correspond to setting time
 actionBtn =Button(
     window,
     text="Establecer fecha",
     padx=10,
     pady=10,
-    command=display_msg
+    command=lambda: (setdatefromcal(), distancebutton())
 )
 actionBtn.pack(pady=10)
 
@@ -163,21 +177,27 @@ actionBtn2 =Button(
     text="Tiempo real",
     padx=10,
     pady=10,
-    command=lambda: timeget.set('now')
+    command=lambda: (timeget.set('now'), distancebutton())
 )
 actionBtn2.pack(pady=10)
 
-msg_display = Label(
+text1 = Label(text='Distancia Tierra-Marte')
+text1.pack()
+#Clock shown
+clock_display = Label(
     window,
     text=""
 )
-msg_display.pack(pady=10)
+clock_display.pack(pady=10)
+
+#Distance shown
+distance_display = Label(
+    window,
+    text=""
+)
+distance_display.pack(pady=10)
 
 
-
-date = Label(window, text = "")
-date.pack(pady = 20)
-text1 = Label(text='Distancia Tierra-Marte')
-text1.pack()
+#Run processes
 distancebutton()
 window.mainloop()
